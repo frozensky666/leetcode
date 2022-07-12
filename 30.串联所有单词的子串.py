bd7af58efcbfb12f15e23edc3822f5ef505eb284
LiXueSong <lixuesong.sky@bytedance.com>
2022-07-12 19:33:47 +0800

    123123

--- 30.串联所有单词的子串.py
+++ 30.串联所有单词的子串.py
@@ -12,5 +12,26 @@ class Solution(object):
         :type words: List[str]
         :rtype: List[int]
         """
+        wordsMap = {}
+        statisticsMap = {}
+        for word in words:
+            if wordsMap.get(word):
+                wordsMap[word] = wordsMap[word] + 1
+            else:
+                wordsMap[word] = 0
+                statisticsMap[word] = 0
+        wordLen = len(words[0])
+        for i in range(0, len(s), wordLen):
+            # TODO
+            pass
+
+    def wordCompare(word, s, index):
+        wordLen = len(word)
+        for i in range(index, index+wordLen):
+            if s[i] != word[i]:
+                return False
+        return True
+        
+                
 # @lc code=end
 
